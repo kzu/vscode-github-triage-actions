dbc160ea9c6d233c38425ce4f6c8cb7220d3ee67
Bhavya U <bhavyau@microsoft.com>
2024-08-23 18:53:34 -0700

    Refactor ReleasePipelineAction to update closed issue query date

--- release-pipeline/index.ts
+++ release-pipeline/index.ts
@@ -23,7 +23,7 @@ class ReleasePipelineAction extends Action {
 	}
 
 	async onTriggered(github: OctoKit) {
-		const query = `is:issue is:closed -label:unreleased -label:insiders-released closed:2024-08-17`;
+		const query = `is:issue is:closed -label:unreleased -label:insiders-released closed:2024-08-23`;
 		safeLog('Query:', query);
 		let count = 1;
 		for await (const page of github.query({ q: query })) {
